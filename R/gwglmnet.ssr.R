2c81f5b1800988622f291977520c820b953d734b
Wesley Brooks <brooks.wesley@gmail.com>
2013-12-16 17:08:29 -0600

    working on nen

--- R/gwglmnet.ssr.R
+++ R/gwglmnet.ssr.R
@@ -1,5 +1,11 @@
 gwglmnet.ssr = function(bw, x, y, family, coords, loc, dist, s, verbose, prior.weights, gweight, target, adapt, precondition, interact, alpha, oracle, shrunk.fit, AICc) {
-    gwglmnet.object = gwglmnet.fit.inner(x=x, y=y, family=family, coords=coords, loc=loc, bw=bw, dist=dist, s=s, verbose=verbose, gwr.weights=NULL, prior.weights=prior.weights, gweight=gweight, adapt=adapt, precondition=precondition, interact=interact, alpha=alpha, oracle=oracle, shrunk.fit=shrunk.fit, AICc=AICc)
+    if (is.null(oracle)) {
+        gwglmnet.object = gwglmnet.fit.inner(x=x, y=y, family=family, coords=coords, loc=loc, bw=bw, dist=dist, s=s, verbose=verbose, gwr.weights=NULL, prior.weights=prior.weights, gweight=gweight, adapt=adapt, precondition=precondition, interact=interact, alpha=alpha, shrunk.fit=shrunk.fit, AICc=AICc)
+    }
+    else {
+        gwglmnet.object = gwselect.fit.oracle(x=x, y=y, family=family, bw=bw, coords=coords, loc=loc, indx=indx, oracle=oracle[[i]], mode.select=mode.select, tuning=tuning, predict=predict, simulation=simulation, verbose=verbose, gwr.weights=NULL, interact=interact, prior.weights=prior.weights, gweight=gweight, AICc=AICc)
+    }
+
     loss = gwglmnet.object[['loss']]
     if (verbose) {cat(paste('loc:(', paste(loc, collapse=","), '), target: ', target, ', bw:', bw, ', ssr:', loss, ', miss:', abs(loss-target), '\n', sep=""))}
     return(abs(loss-target))
