04025309e2455cad45863077e38a75d017fb8ebd
Wesley Brooks <brooks.wesley@gmail.com>
2014-01-31 18:23:33 -0600

    added trH to the trace of bandwidth selection

--- R/gwglmnet.cv.f.r
+++ R/gwglmnet.cv.f.r
@@ -11,13 +11,12 @@ gwglmnet.cv.f = function(formula, data, weights, indx, family, bw, coords, gweig
     if (bw.select=='AICc') {
         #trH = sum(sapply(gwglmnet.model[['model']][['models']], function(x) {tail(x[['tunelist']][['trace.local']],1)}))
        	trH = sum(sapply(gwglmnet.model[['model']][['models']], function(x) {tail(x[['tunelist']][['df-local']],1)}))
-cat(paste("total df: ", round(trH,4), "\n", sep=""))
        	loss = nrow(data) * (log(mean(sapply(gwglmnet.model[['model']][['models']], function(x) {x[['tunelist']][['ssr-loc']][[resid.type]]}))) + 1 + (2*(trH+1))/(nrow(data)-trH-2) + log(2*pi))
     } else if (bw.select=='GCV') {
         trH = sum(sapply(gwglmnet.model[['model']][['models']], function(x) {tail(x[['tunelist']][['trace.local']],1)})) 
         loss = sum(sapply(gwglmnet.model[['model']][['models']], function(x) {x[['tunelist']][['ssr-loc']][[resid.type]]})) / (nrow(data)-trH)**2
     } else if (bw.select=='BICg') {
-        loss = sum(sapply(gwglmnet.model[['model']][['models']], function(x) {
+        trH = sum(sapply(gwglmnet.model[['model']][['models']], function(x) {
             s2 = x[['tunelist']][['s2']]
             if (family=='gaussian') { ll = min(x[['tunelist']][['ssr-loc']][[resid.type]])/s2 + log(s2) }
             else if (family=='binomial') { ll = min(x[['tunelist']][['ssr-loc']][[resid.type]]) }
@@ -25,15 +24,15 @@ cat(paste("total df: ", round(trH,4), "\n", sep=""))
             df = v[['tunelist']][['df']]
             return(ll + log(x[['tunelist']][['n']]) * df / x[['tunelist']][['n']])
             }))
-        loss = loss + sum(sapply(gwglmnet.model[['model']][['models']], function(x) {min(x[['tunelist']][['ssr-loc']][[resid.type]])}))
+        loss = trH + sum(sapply(gwglmnet.model[['model']][['models']], function(x) {min(x[['tunelist']][['ssr-loc']][[resid.type]])}))
         #"Simplistic" BIC - based on eq4.22 from the Fotheringham et al. book:
         #loss = nrow(data) * (log(mean(sapply(gwglmnet.model[['model']][['models']], function(x) {x[['ssr.local']]}))) + 1 + log(2*pi)) + trH * log(nrow(data))/2
     }
 
-    res = mget('trace', env=env, ifnotfound=list(matrix(NA, nrow=0, ncol=2)))
-    res$trace = rbind(res$trace, c(bw, loss))
+    res = mget('trace', env=env, ifnotfound=list(matrix(NA, nrow=0, ncol=3)))
+    res$trace = rbind(res$trace, c(bw, loss, trH))
     assign('trace', res$trace, env=env)
 
-    cat(paste('Bandwidth: ', round(bw, 3), '. Loss: ', signif(loss, 5), '\n', sep=''))
+    cat(paste('Bandwidth: ', round(bw, 3), '. df: ', round(trH,4), '. Loss: ', signif(loss, 5), '\n', sep=''))
     return(loss)
 }
