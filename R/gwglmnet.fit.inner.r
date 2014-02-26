6e75a4bc50bdcad936af92d5134a1ecdb141c8eb
Wesley Brooks <brooks.wesley@gmail.com>
2014-02-26 11:57:31 -0600

    'residuals' is a string, not a variable

--- R/gwglmnet.fit.inner.r
+++ R/gwglmnet.fit.inner.r
@@ -148,8 +148,8 @@ print(vars)
             if (mode.select == 'AIC') {penalty = 2*df}
             if (mode.select == 'AICc') {penalty = 2*df*(df-1)/(sum(w) - df - 1)}
             if (mode.select == 'BIC') {penalty = sum(w[permutation])*df}
-print(model[[residuals]][colocated,])
-            loss = (model[[residuals]][colocated,])**2 + penalty*w[permutation][colocated] / sum(w[permutation])
+print(model[['residuals']][colocated,])
+            loss = (model[['residuals']][colocated,])**2 + penalty*w[permutation][colocated] / sum(w[permutation])
             
             #Pick the lambda that minimizes the loss:
             k = which.min(loss)
